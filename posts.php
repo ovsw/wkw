678f97a81aa8a4b540bf6057fdea07243214f2c3
Ovi Savescu <ovi@ovswebsites.com>
2018-06-12 05:25:17 +0300

    search page

--- posts.php
+++ posts.php
@@ -35,6 +35,34 @@
 
       <?php endforeach; ?>
 
+      <!--       <div class="col-lg-12 col-md-12 col-sm-12 one-column a-a-a">
+        <div class="item matchHeight">
+
+          <?php $image = $post->images->first(); $img = $image->size(768, 512) ?>
+          <a href="<?= $post->url ?>" class="photo"><img src="<?= $img->url ?>" class="full-width" alt="Blog"></a>
+
+          <div class="description">
+              <a href="<?= $post->url ?>" class="header"><h4><?= $post->title ?></h4></a>
+              <p class="text"><?= $sanitizer->truncate($post->body) ?></p>
+          </div>
+
+          <div class="blog-info">
+              <div class="row">
+                  <div class="col-lg-6 col-sm-12  col-xs-6">
+                      <a href="<?= $post->url ?>" class="date"><?= date("d M 'y", $post->getUnformatted("date")); ?></a>
+                  </div>
+                  <div class="col-lg-6 col-sm-12 col-xs-6 right">
+                      <ul>
+                          <!-- <li class="icon-fav"><a href="#"><span class="fa fa-eye"></span>16</a></li> -->
+                          <li class="icon-comments"><a href="#"><span class="fa fa-commenting-o"></span><?= count($page->comments) ?></a></li>
+                      </ul>
+                  </div>
+              </div>
+          </div>
+
+        </div>
+      </div> -->
+
       <?php       
       echo $posts->renderPager(array(
       'nextItemLabel' => "Next",
@@ -58,3 +86,5 @@
 
 </div>
 <!-- END SIDEBAR -->
+
+
